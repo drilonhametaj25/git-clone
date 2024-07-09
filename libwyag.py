9e841ad965792a6aecd2f0a226524713cf4b4623
drilon <drilonhametaj25@gmail.com>
2024-07-09 11:33:49 +0200

    feat: utility function

--- libwyag.py
+++ libwyag.py
@@ -70,3 +70,25 @@ def repo_path(repo, *path):
     """Compute path under repo's gitdir."""
     return os.path.join(repo.gitdir, *path)
 
+def repo_file(repo, *path, mkdir=False):
+    """Same as repo_path but create dirname if absent"""
+
+    if repo_dir(repo, *path[:-1], mkdir=mkdir):
+        return repo_path(repo, *path)
+    
+def repo_dir(repo, *path, mkdir=False):
+    """Same as repo_path but mkdir *path if asbent if mkdir"""
+
+    path = repo_path(repo, *path)
+
+    if os.path.exists(path):
+        if (os.path.isdir(path)):
+            return path
+        else:
+            raise Exception("Not a direcotry %s" % path)
+        
+    if mkdir:
+        os.mkdir(path)
+        return path
+    else:
+        return None
